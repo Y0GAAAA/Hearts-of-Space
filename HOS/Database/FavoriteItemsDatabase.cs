0691959989bf908535ac0b86248e6ec3151414ac
Y0GAAAA <35837405+Y0GAAAA@users.noreply.github.com>
2022-04-22 23:54:41 +0200

    Create favorite elements wrapper class

---
+++ HOS/Database/FavoriteItemsDatabase.cs
@@ -0,0 +1,90 @@
+﻿using Global.Util;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Database
+{
+    public class FavoriteItemsDatabase
+    {
+        #region PATHS
+        private static readonly string DATABASE_DIRECTORY = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "Hearts of Space"
+        );
+        private static readonly string DATABASE_FILENAME = Path.Combine(
+            DATABASE_DIRECTORY,
+            "favorites.db"
+        );
+        #endregion
+
+        #region SQL STRING
+        private const string CREATE_TABLES =
+        @"
+        CREATE TABLE IF NOT EXISTS albums (
+            id INTEGER NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS programs (
+            id INTEGER NOT NULL
+        );
+        ";
+        private string GetTableName(ItemType type) => type switch
+        {
+            ItemType.Album => "albums",
+            ItemType.Program => "programs",
+            _ => throw new InvalidOperationException("unreachable"),
+        };
+        private string AddSqlString(ItemType type) => $"INSERT INTO {GetTableName(type)}(id) VALUES(@id);";
+        private string SelectSqlString(ItemType type) => $"SELECT * FROM {GetTableName(type)};";
+        #endregion
+
+        private readonly SqliteConnection connection;
+        
+        public enum ItemType
+        {
+            Album,
+            Program,
+        }
+
+        public FavoriteItemsDatabase()
+        {
+            _ = Directory.CreateDirectory(DATABASE_DIRECTORY);
+
+            connection = new SqliteConnection($"Data Source={DATABASE_FILENAME}");
+            connection.Open();
+
+            GetCommand(CREATE_TABLES).ExecuteNonQuery();
+        }
+
+        private SqliteCommand GetCommand(string sql)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = sql;
+            return command;
+        }
+
+        public async Task Add(ItemType type, int id)
+        {
+            string sql = AddSqlString(type);
+            var command = GetCommand(sql);
+            command.Parameters.AddWithValue("@id", id);
+            await command.ExecuteNonQueryAsync();
+        }
+        public async Task<Int32[]> Get(ItemType type)
+        {
+            string sql = SelectSqlString(type);
+            var reader = await GetCommand(sql).ExecuteReaderAsync();
+            var list = new List<Int32>();
+            list.AddWhile(
+                () => reader.Read(), 
+                () => reader.GetInt32(0)
+            );
+            return list.ToArray();
+        }
+
+    }
+}
