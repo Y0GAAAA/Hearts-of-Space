44a866e8fcf3c27f1c0b0306f918a036e891f2e4
Y0GAAAA <35837405+Y0GAAAA@users.noreply.github.com>
2022-04-24 16:10:25 +0200

    Add helper method to construct rows

---
+++ Global/Util/DataTableExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace Global.Util
+{
+    public static class DataTableExtension
+    {
+        public static DataRow NewRow(this DataTable table, params object[] obj)
+        {
+            var row = table.NewRow();
+            row.ItemArray = obj;
+            return row;
+        }
+    }
+}
