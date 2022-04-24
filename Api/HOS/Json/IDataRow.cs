9512afb165bdad48c81a1ed8a94d12a0b357b7d9
Y0GAAAA <35837405+Y0GAAAA@users.noreply.github.com>
2022-04-24 16:38:25 +0200

    Add DataRow array extension

--- Api/HOS/Json/IDataRow.cs
+++ Api/HOS/Json/IDataRow.cs
@@ -2,8 +2,17 @@
 
 namespace Api.HOS.Json
 {
-    internal interface IDataRow
+    public interface IDataRow
     {
         DataRow GetRow();
+    }
+    public static class IDataRowExtension
+    {
+        public static DataTable ToDataTable(this IDataRow[] rows, DataTable table)
+        {
+            for (int i = 0; i < rows.Length; i++)
+                table.Rows.Add(rows[i].GetRow());
+            return table;
+        }
     }
 }
