72ef2bc882d7f8b27ed8817c55c5240615c3e15d
Y0GAAAA <35837405+Y0GAAAA@users.noreply.github.com>
2022-05-07 18:20:28 +0200

    Remove  IDataRow interface if condition not met

--- Api/HOS/Json/AlbumCollection.cs
+++ Api/HOS/Json/AlbumCollection.cs
@@ -13,13 +13,17 @@ namespace Api.HOS.Json
         public int totalElements { get; set; }
     }
 
-    public class Album : IDataRow
+    public class Album
+#if _HOSLIB
+    : IDataRow
+#endif
     {
         public int id { get; set; }
         public string attribution { get; set; }
         public string title { get; set; }
         public int duration { get; set; }
-
+#if _HOSLIB
         public object[] GetRow() => new object[] { id, title, attribution, duration };
+#endif
     }
 }
--- Api/HOS/Json/ChannelCollection.cs
+++ Api/HOS/Json/ChannelCollection.cs
@@ -4,21 +4,29 @@ using System.Data;
 
 namespace Api.HOS.Json
 {
-    public class Channel : IDataRow
+    public class Channel
+#if _HOSLIB
+    : IDataRow
+#endif
     {
         public int id { get; set; }
         public string name { get; set; }
         public string description { get; set; }
         public ChannelProgram[] programs { get; set; }
-
+#if _HOSLIB
         public object[] GetRow() => new object[]{ id, name, description };
+#endif
     }
-    public class ChannelProgram : IDataRow
+    public class ChannelProgram
+#if _HOSLIB
+    : IDataRow
+#endif
     {
         public int id { get; set; }
         public string title { get; set; }
         public string shortDescription { get; set; }
-
+#if _HOSLIB
         public object[] GetRow() => new object[]{ id, title, shortDescription};
+#endif
     }
 }
